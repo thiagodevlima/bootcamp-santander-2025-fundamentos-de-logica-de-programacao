acaeeb58e862c280babb14b20ba7b246ee650b58
thiagodevlima <thiagodlima@gmail.com>
2026-01-02 00:00:26 -0300

    renomeei o arquivo e adicionei comentários ao código

--- classificador-nivel-heroi.js
+++ classificador-nivel-heroi.js
@@ -1,3 +1,4 @@
+// Lista de heróis com seus respectivos pontos de experiência (XP)
 let herois = [
   { nome: "Aragorn", xp: 750 },
   { nome: "Legolas", xp: 3500 },
@@ -5,9 +6,11 @@ let herois = [
   { nome: "Gandalf", xp: 12000 },
 ];
 
+// Percorre cada herói da lista
 for (let heroi of herois) {
-  let nivel = "";
+  let nivel = ""; // Variável para armazenar o nível do herói
 
+  // Classificação do nível com base na quantidade de XP
   if (heroi.xp < 1000) nivel = "Ferro";
   else if (heroi.xp <= 2000) nivel = "Bronze";
   else if (heroi.xp <= 5000) nivel = "Prata";
@@ -15,7 +18,8 @@ for (let heroi of herois) {
   else if (heroi.xp <= 8000) nivel = "Platina";
   else if (heroi.xp <= 9000) nivel = "Ascendente";
   else if (heroi.xp <= 10000) nivel = "Imortal";
-  else nivel = "Radiante";
+  else nivel = "Radiante"; // Para XP acima de 10.000
 
+  // Exibe no console o nome do herói e seu nível correspondente
   console.log(`O Herói de nome ${heroi.nome} está no nível de ${nivel}`);
 }
